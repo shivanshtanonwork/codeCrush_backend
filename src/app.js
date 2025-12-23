9fdb518bf0068a6d9aaef17b8829467bb86f2426
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-12-23 17:01:50 +0530

    GET, POST and Delete HTTP methods and testing them using Postman

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const express = require("express");
 
 const app = express();
 
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shivansh", lastName: "Tandon" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save data to DB");
+  res.send("Data successfully saved to the DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully!");
+});
+// This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
 });
-app.use("/hello", (req, res) => {
-  res.send("hello hello hello");
-});
+
 app.listen(3000, () => {
   console.log(`App is listening on port 3000`);
 });
